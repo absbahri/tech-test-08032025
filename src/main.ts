b5252b78ac47a7c2eddc995078504c9a0e1b3e58
Abdul Bahri <abdulbahri2000@yahoo.co.uk>
2025-03-08 19:48:10 +0000

    added required plugins

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,19 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { createMemoryHistory, createRouter } from 'vue-router'
+
+import ListView from './pages/List.vue'
+import ShipView from './pages/Ship.vue'
+
+const routes = [
+  { path: '/', component: ListView },
+  { path: '/ship/:id', component: ShipView },
+]
+
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+})
+
+createApp(App).use(router).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath, URL } from 'url'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@layouts': fileURLToPath(new URL('./src/layouts', import.meta.url)),
+    }
+  }
 })
